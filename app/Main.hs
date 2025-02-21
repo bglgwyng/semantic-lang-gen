86f6a1ce59b6189b3edec6d1bcd34a06f00cba51
bgl gwyng <bgl@gwyng.com>
2025-02-21 20:17:43 +0900

    Initial commit.

---
+++ app/Main.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+module Main (main) where
+
+import           TreeSitter.Parser
+import           TreeSitter.Tree
+import           TreeSitter.Go
+import           TreeSitter.Node
+import           Foreign.C.String
+import           Foreign.Ptr                    ( Ptr
+                                                , nullPtr
+                                                )
+import           Foreign.Marshal.Alloc          ( malloc )
+import           Foreign.Marshal.Array          ( mallocArray )
+import           Foreign.Storable               ( peek
+                                                , peekElemOff
+                                                , poke
+                                                )
+import           Control.Monad
+import AST.Unmarshal (parseByteString)
+import Language.Go.AST
+
+main :: IO ()
+main = do
+  parser <- ts_parser_new
+  
+  _ <- ts_parser_set_language parser tree_sitter_go
+  let source =
+        "package main\n" ++
+        "import \"fmt\"\n" ++
+        "func main() {\n" ++
+        "    fmt.Println(\"hello world\")\n" ++
+        "}"
+  let source2  =
+        "package main\n"
+         <>
+        "import \"fmt\"\n" <>
+        "func main() {  \n}"
+        -- <>
+        -- "    fmt.Println()\n" <>
+        -- "}\n"
+  x <- parseByteString @SourceFile @() tree_sitter_go source2
+  -- case x of 
+  --   SourceFile {..} -> do
+  --     putStrLn "module (root) ------------"
+  --   _ -> putStrLn "not a source file"
+  print x
+  (str, len) <- newCStringLen source
+  tree       <- ts_parser_parse_string parser nullPtr str len
+
+  n          <- malloc
+  ts_tree_root_node_p tree n
+
+  putStrLn "module (root) ------------"
+  Node {..} <- peek n  -- header, imports, and declarations
+  let childCount = fromIntegral nodeChildCount
+
+  children <- mallocArray childCount
+  tsNode   <- malloc
+  poke tsNode nodeTSNode
+  ts_node_copy_child_nodes tsNode children
+
+  printChildren children childCount
+
+  putStrLn "declarations ------------"
+  Node {..} <- peekElemOff children 2  -- declarations: bind and bind
+  let nextChildCount = fromIntegral nodeChildCount
+
+  nextChildren <- mallocArray nextChildCount
+  nextTsNode   <- malloc
+  poke nextTsNode nodeTSNode
+  ts_node_copy_child_nodes nextTsNode nextChildren
+
+  printChildren nextChildren nextChildCount
+
+  putStrLn "END"
+
+printChildren :: Ptr Node -> Int -> IO ()
+printChildren children count = forM_
+  [0 .. count - 1]
+  (\n -> do
+    child <- peekElemOff children n
+    printNode child
+  )
+
+printNode :: Node -> IO ()
+printNode n@(Node {..}) = do
+  theType <- peekCString nodeType
+  let TSPoint {..} = nodeStartPoint n
+      start        = "(" ++ show pointRow ++ "," ++ show pointColumn ++ ")"
+  let TSPoint {..} = nodeEndPoint
+      end          = "(" ++ show pointRow ++ "," ++ show pointColumn ++ ")"
+  putStrLn $ theType ++ start ++ "-" ++ end
---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "dEsCrIpTiOn";
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys =
+      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+  };
+
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix, ... }:
+    let
+      name = "nAmE";
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            ${name} = final.haskell-nix.cabalProject'
+              {
+                src = ./.;
+                supportHpack = true;
+                compiler-nix-name = "ghc948";
+                shell.tools = {
+                  cabal = "latest";
+                  haskell-language-server = "latest";
+                  hlint = "latest";
+                  # stylish-haskell = "latest차";
+                  # hpack = "latest";
+                };
+              };
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        flake = pkgs.${name}.flake { };
+      in
+      (flake // rec {
+        packages.default = flake.packages."${name}:exe:app";
+        devShells.default = flake.devShell.overrideAttrs (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs ++ [
+            pkgs.tree-sitter
+            pkgs.nodejs_22
+          ];
+        });
+      }));
+}
